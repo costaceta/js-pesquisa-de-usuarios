3477ad6ec08832577d8339a73db6668efdb25450
Rafael Costa <rcostadesignweb@gmail.com>
2020-08-17 21:55:08 -0300

    feat: carregar dados da api de usuários

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,40 @@
-console.log('Script running!')
+
+// https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo
+
+let allUsers = [];
+
+window.addEventListener('load', () => {
+    fetchUsers();
+
+    console.log(allUsers)
+})
+
+async function fetchUsers () {
+
+    try {
+        const response = await fetch("https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo")
+        const json     = await response.json();
+
+        const { results } = json;
+
+        allUsers = results.map( user => {
+            const {
+                name: { first, last },
+                dob: { age },
+                picture: { thumbnail }
+            } = user;
+
+            return {
+                name: `${first} ${last}`,
+                age,
+                thumbnail,
+            }
+        })
+
+        console.log(allUsers)
+
+    } catch(error) {
+        console.log(error)
+    }
+
+}
