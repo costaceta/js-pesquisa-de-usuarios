887ac3347843444ee11be56d5d93e0aba888931a
Rafael Costa <rcostadesignweb@gmail.com>
2020-08-20 20:24:53 -0300

    Fetch users

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,7 @@
-
-// https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo
-
 let allUsers = [];
 
 window.addEventListener('load', () => {
     fetchUsers();
-
-    console.log(allUsers)
 })
 
 async function fetchUsers () {
